d964e541825e02b34e27af0fbf123888622d315c
wk2197727@163.com <wk2197727@163.com>
2019-07-29 09:09:08 +0800

    add  adapter demo

---
+++ kmybaties/src/main/java/com/java/main/pattern/adapter/AdapterTest.java
@@ -0,0 +1,13 @@
+package com.java.main.pattern.adapter;
+
+/**
+ * @Auther: kai2.wang
+ * @Date: 2019/7/29 09:08
+ * @Description:
+ */
+public class AdapterTest {
+
+    public static void main(String[] args) {
+
+    }
+}
