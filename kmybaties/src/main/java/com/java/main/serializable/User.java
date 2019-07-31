98e831889f6ee81929bbe32d2f60341085aad45a
wk2197727@163.com <wk2197727@163.com>
2019-07-31 16:51:35 +0800

    add serializable

---
+++ kmybaties/src/main/java/com/java/main/serializable/User.java
@@ -0,0 +1,14 @@
+package com.java.main.serializable;
+
+import lombok.Data;
+
+/**
+ * @Auther: kai2.wang
+ * @Date: 2019/7/31 16:50
+ * @Description:
+ */
+@Data
+public class User {
+    private Integer id;
+    private String name;
+}
